f3ea9a3d4efece50692aa365e8de3335daba7de9
benjyalper <benjyalper@hotmail.com>
2023-07-02 16:59:44 +0300

    stuff

--- script.js
+++ script.js
@@ -11,6 +11,11 @@ $('#capibara').on("click", function () {
 });
 
 
+var cellNumber = 7
+
+$(`.background-container > .background-item:nth-child(${cellNumber})`).css("border", "5px solid salmon");
 
 
-
+// .background-container>.background-item:nth-child(1) {
+//     border: 5px solid salmon;
+// }
