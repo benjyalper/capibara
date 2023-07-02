89f2cf2af5901ef3afa263b3d94f82aaf21a8c61
benjyalper <benjyalper@hotmail.com>
2023-07-02 12:49:28 +0300

    stuff

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // $("h1").css("color", "blue");
-$(".background-item").on("click", function () {
-    $(".background-item").addClass("highlighted");
-})
+// $(".background-item").on("click", function () {
+//     $(".background-item").addClass("highlighted");
+// });
+
+//turning capibara
+
+$('#capibara').on("click", function () {
+    $('#capibara').toggleClass('turnRight');
+
+});
+
+
+
+
 
