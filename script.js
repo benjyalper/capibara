2c528803b13a83aaf5838e4f42faadbf0d9f5f6c
benjyalper <benjyalper@hotmail.com>
2023-07-02 22:05:12 +0300

    stuff

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// $("h1").css("color", "blue");
-// $(".background-item").on("click", function () {
-//     $(".background-item").addClass("highlighted");
-// });
 
 //turning capibara
 
@@ -10,10 +6,18 @@ $('#capibara').on("click", function () {
 
 });
 
+var img = $('<img id="capibara" src="./images/capibara-happy2.png"></img>')
 
-var cellNumber = 7
+//select grid cell
+$(document).on('click', '.background-item', function () {//(check the second parameter because we usualy have 2)
+    // Remove the "selected" class from all grid cells
+    $('.background-item').removeClass('selected');
 
-$(`.background-container > .background-item:nth-child(${cellNumber})`).css("border", "5px solid salmon");
+    // Add the "selected" class to the clicked grid cell
+    $(this).addClass('selected');
+    $(this).append(img);//why is the last img erasing?
+});
+
 
 
 // .background-container>.background-item:nth-child(1) {
